9e88cbb3eca6aea1051e555360ca966929d185dc
박예인 <98065254+iasfk@users.noreply.github.com>
2022-11-15 16:17:13 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.48 ms, Memory: 53.4 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -3,35 +3,48 @@ import java.util.*;
 class Solution {
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static int[][] map, visited;
+    static int[][] map;
+    static boolean[][] visited;
     public int solution(int[][] maps) {
         map = maps;
-        visited = new int[maps.length][maps[0].length];
-        bfs();
-        return visited[maps.length-1][maps[0].length-1] == 0 ? -1 : visited[maps.length-1][maps[0].length-1]+1;
+        visited = new boolean[maps.length][maps[0].length];
+        return bfs();
     }
     
-    static void bfs(){
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0,0});
+    static int bfs(){
+        Queue<Node> q = new LinkedList<>();
+        visited[0][0] = true;
+        q.add(new Node(0,0,1));
         while (!q.isEmpty()){
-            int[] position = q.poll();
+            Node nd = q.poll();
             
             for (int i =0; i < 4; i++){
                 
-            int dr = dx[i] + position[0];
-            int dc = dy[i] + position[1];
+            int dr = dx[i] + nd.x;
+            int dc = dy[i] + nd.y;
                 
-            if (dr <0 || dc < 0 || dr>= map.length || dc>= map[0].length)
+            if (dr <0 || dc < 0 || dr>= map.length || dc>= map[0].length|| visited[dr][dc] || map[dr][dc] == 0)
                 continue;
-            
-            if (visited[dr][dc] == 0 && map[dr][dc] == 1){
-                visited[dr][dc] = visited[position[0]][position[1]] +1;
-                q.add(new int[]{dr, dc});
-            }
+            if (dr == map.length-1 && dc == map[0].length-1)
+                return nd.depth+1;
+                
+            visited[dr][dc] = true;
+            Node ne = new Node(dr, dc, nd.depth+1);
+            q.add(ne);
+            }  
         }
-            
-        }
+        return -1;
+    }
+}
 
+class Node {
+    int x;
+    int y;
+    int depth;
+    
+    Node(int x, int y, int depth){
+        this.x =x;
+        this.y =y;
+        this.depth = depth;
     }
 }
