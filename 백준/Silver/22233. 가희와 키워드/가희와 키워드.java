63cde01f337ff4ecf0d5402ce9a1244262c5e53c
박예인 <98065254+iasfk@users.noreply.github.com>
2022-12-22 21:34:35 +0900

    [Silver II] Title: 가희와 키워드, Time: 1160 ms, Memory: 272008 KB -BaekjoonHub

--- 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
+++ 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
@@ -6,19 +6,21 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		HashSet<String> set = new HashSet<>();
-		StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
 		for (int i=0; i<n; i++) {
 			set.add(br.readLine());
 		}
+		int cnt = n;
 		for (int i=0; i<m; i++) {
 			String[] str = br.readLine().split(",");
 			for (String s : str) {
 				if (set.contains(s)) 
-					set.remove(s);
+					{set.remove(s);
+					cnt--;}
 			}
-			sb.append(set.size()).append('\n');
+			sb.append(cnt).append('\n');
 		}
 		System.out.println(sb);
 	}
